3a858d63a042a26d012efa5a7a782fdc745464bd
Ali <a.kahawati@student.tudelft.nl>
2017-12-01 22:15:49 +0100

    no message

---
+++ src/test/java/QuestionListTest.java
@@ -0,0 +1,65 @@
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.util.ArrayList;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+public class QuestionListTest {
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Test
+    public void test_the_constructor() {
+        ArrayList<String> al = new ArrayList<>();
+        Question q = new Question(1,al,"Some question?");
+        QuestionList ql = new QuestionList();
+
+        assertThat("Question constructor is not working well", new Question(1, al, "What is what?").getQuestion(),
+                equalTo("What is what?"));
+    }
+
+    @Test
+    public void test_the_addQuestions() {
+        ArrayList<String> al = new ArrayList<>();
+        Question q = new Question(1,al,"Some question?");
+        QuestionList ql = new QuestionList();
+        ql.add(q);
+        QuestionList q2 = new QuestionList();
+        q2.add(q);
+       assertTrue("Not working well!",q2.equals(ql));
+    }
+
+    @Test
+    public void test_the_addQuestions1() {
+        ArrayList<String> al = new ArrayList<>();
+        Question q = new Question(1,al,"Some question?");
+        Question qu = new Question(2,al,"Another Question?");
+        QuestionList ql = new QuestionList();
+        ql.add(q);
+        QuestionList q2 = new QuestionList();
+        q2.add(q);
+        q2.add(qu);
+        assertFalse("Add is not working well!",q2.equals(ql));
+    }
+
+    @Test
+    public void test_the_addQuestions2() {
+        ArrayList<String> al = new ArrayList<>();
+        Question q = new Question(1,al,"Some question?");
+        Question qu = new Question(2,al,"Another Question?");
+        QuestionList ql = new QuestionList();
+        ql.add(q);
+        ql.add(qu);
+        QuestionList q2 = new QuestionList();
+        q2.add(q);
+        q2.add(qu);
+        assertTrue("Add is not working well!",q2.equals(ql));
+    }
+
+}
--- src/test/java/QuestionTest.java
+++ src/test/java/QuestionTest.java
@@ -1,3 +1,4 @@
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -11,18 +12,115 @@ public class QuestionTest {
     @Rule
     public ExpectedException expectedException = ExpectedException.none();
 
+    /**
+     * Test for the getter of the Question
+     */
     @Test
     public void test_the_constructor() {
         ArrayList<String> al = new ArrayList<>();
-        assertThat("Question constructer is not working well", new Question(1, al, "What is what?").getQuestion(),
+        assertThat("Question constructor is not working well", new Question(1, al, "What is what?").getQuestion(),
                 equalTo("What is what?"));
     }
 
+    /**
+     * Test for the getter of the RightAnswer
+     */
     @Test
     public void test_the_constructor1() {
         ArrayList<String> al = new ArrayList<>();
         assertTrue("Address constructor is not working",
-                new Question (2, al, "What is what?").getRightAnswer() == 2);
+                new Question(2, al, "What is what?").getRightAnswer() == 2);
     }
 
+    /**
+     * A test for AnswerList getter
+     */
+    @Test
+    public void test_the_constructor2() {
+        ArrayList<String> la = new ArrayList<>();
+        ArrayList<String> al = new ArrayList<>();
+        String a = "answer b";
+        String a1 = "answer a";
+        String a2 = "answer c";
+        al.add(a); al.add(a1); al.add(a2);
+        la.add(a);
+        assertFalse("", new Question(1, al, "some question?").getAnswerList().equals(la));
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void test_print(){
+        ArrayList<String> al = new ArrayList<>();
+        String a = "answer b";
+        String a1 = "answer a";
+        String a2 = "answer c";
+        Question q = new Question(2,al,"What is the right answer?");
+
+    }
+
+    /**
+     *All 5 next tests are to test the method of isitRightAnswer
+     *
+     */
+    @Test
+    public void test_isItRightAnswer(){
+        ArrayList<String> al = new ArrayList<>();
+        String a = "answer b";
+        String b = "answer a";
+        String c = "answer c";
+        String d = "answer d";
+        al.add(a); al.add(b); al.add(c); al.add(d);
+        Question q = new Question(2,al,"What is the right answer?");
+        assertTrue("Isrightanswer is not working!",q.isItRightAnswer('b'));
+    }
+
+    @Test
+    public void test_isItRightAnswer1(){
+        ArrayList<String> al = new ArrayList<>();
+        String a = "answer b";
+        String b = "answer a";
+        String c = "answer c";
+        String d = "answer d";
+        al.add(a); al.add(b); al.add(c); al.add(d);
+        Question q = new Question(2,al,"What is the right answer?");
+        assertFalse("Isrightanswer is not working!",q.isItRightAnswer('d'));
+    }
+
+    @Test
+    public void test_isItRightAnswer2(){
+        ArrayList<String> al = new ArrayList<>();
+        String a = "answer b";
+        String b = "answer a";
+        String c = "answer c";
+        String d = "answer d";
+        al.add(a); al.add(b); al.add(c); al.add(d);
+        Question q = new Question(2,al,"What is the right answer?");
+        assertFalse("Isrightanswer is not working!",q.isItRightAnswer('e'));
+    }
+
+    @Test
+    public void test_isItRightAnswer3(){
+        ArrayList<String> al = new ArrayList<>();
+        String a = "answer b";
+        String b = "answer a";
+        String c = "answer c";
+        String d = "answer d";
+        al.add(a); al.add(b); al.add(c); al.add(d);
+        Question q = new Question(4,al,"What is the right answer?");
+        assertTrue("Isrightanswer is not working!",q.isItRightAnswer('d'));
+    }
+
+    @Test
+    public void test_isItRightAnswer4(){
+        ArrayList<String> al = new ArrayList<>();
+        String a = "answer b";
+        String b = "answer a";
+        String c = "answer c";
+        String d = "answer d";
+        al.add(a); al.add(b); al.add(c); al.add(d);
+        Question q = new Question(1,al,"What is the right answer?");
+        assertFalse("Isrightanswer is not working!",q.isItRightAnswer('z'));
+    }
 }
