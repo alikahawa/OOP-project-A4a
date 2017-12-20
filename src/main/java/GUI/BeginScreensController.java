75626033e332828e6e5d8c4bb2164d77599f5042
Raymond Koopmanschap <r.koopmanschap@student.tudelft.nl>
2017-12-20 16:58:51 +0100

    Linked to Welcome to the Loginpage and from the loginpage the create account button to the Userchoice3.fxml

---
+++ src/main/java/GUI/BeginScreensController.java
@@ -0,0 +1,37 @@
+package GUI;
+
+import Application.MultiQuestion;
+import Application.Question;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+public class BeginScreensController {
+
+    @FXML private Button start;
+    @FXML private Button login;
+    @FXML private Button createAccount;
+
+
+    public void fromWelcomeToLoginScreen() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Loginpage1.fxml"));
+        Parent root = loader.load();
+        BeginScreensController controller = loader.getController();
+        start.getScene().setRoot(root);
+    }
+
+    public void createAccountButton() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Userchoice3.fxml"));
+        Parent root = loader.load();
+        BeginScreensController controller = loader.getController();
+        createAccount.getScene().setRoot(root);
+    }
+
+
+}
