6845f604a4d1fd9603e9909a30f258d999c5e176
Matthias Meester <matthiasmeester@gmail.com>
2018-01-12 10:19:34 +0100

    First concept of fxml structure

---
+++ src/main/java/GUI/ClickablePictureTestController.java
@@ -0,0 +1,29 @@
+package GUI;
+
+import Application.MultiQuestion;
+import Application.Question;
+import Application.QuestionList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+public class ClickablePictureTestController {
+
+    @FXML
+    public void initialize() {
+
+        System.out.println("ClickablePicture-based test is loaded");
+
+        //displayQuestion();
+    }
+
+    
+}
--- src/main/java/GUI/TestSelectorController.java
+++ src/main/java/GUI/TestSelectorController.java
@@ -16,8 +16,9 @@ public class TestSelectorController {
 
     @FXML private Button TextBasedExamButton;
     @FXML private Button PictureBasedExamButton1;
-    @FXML private Button PictureBasedExamButton2;
+    @FXML private Button ClickablePictureBasedExamButton;
     @FXML private Button UIBasedExamButton;
+
 
     public void goToStartOfTextBasedTest() throws IOException {
         System.out.println("Text-based test is being loaded");
@@ -26,4 +27,13 @@ public class TestSelectorController {
         TextTestController controller = loader.getController();
         TextBasedExamButton.getScene().setRoot(root);
     }
+
+    public void goToStartOfClickablePictureBasedTest() throws IOException {
+        System.out.println("ClickablePicture-based test is being loaded");
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/ClickablePictureExam.fxml"));
+        Parent root = loader.load();
+        ClickablePictureTestController controller = loader.getController();
+        ClickablePictureBasedExamButton.getScene().setRoot(root);
+    }
+
 }
